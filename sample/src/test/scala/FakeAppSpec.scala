965951465bbc199231c3e65d5995441173c7dc37
Bùi Việt Thành <thanhbv@sandinh.net>
2021-09-28 22:56:16 +0700

    FakeAppSpec "opens java.lang" error on java 16+ only occurred in play 2.6.x

--- sample/src/test/scala/FakeAppSpec.scala
+++ sample/src/test/scala/FakeAppSpec.scala
@@ -2,6 +2,7 @@ import com.google.common.util.concurrent.ExecutionError
 import com.google.inject.CreationException
 import org.specs2.mutable.Specification
 import play.api.inject.guice.GuiceApplicationBuilder
+import play.core.PlayVersion
 
 class FakeAppSpec extends Specification {
   private def javaVersion: Int = scala.sys
@@ -10,15 +11,13 @@ class FakeAppSpec extends Specification {
     .dropWhile(_ == "1")
     .head
     .toInt
+  private def isPlay26 = PlayVersion.current.startsWith("2.6.")
 
   """FakeApplication (and `WithApplication`) can't start, even with this setting in reference.conf:
     | play.modules.disabled += "play.api.db.evolutions.EvolutionsModule"
     | """.stripMargin >> {
 
-    if (javaVersion < 16) {
-      GuiceApplicationBuilder().build() must not(throwAn[CreationException])
-    }.pendingUntilFixed("See issue #1")
-    else {
+    if (isPlay26 && javaVersion >= 16) {
       // ExecutionError: Fatal execution error, caused by java.lang.ExceptionInInitializerError
       // CAUSED BY
       // InaccessibleObjectException: Unable to make protected final java.lang.Class java.lang.ClassLoader.defineClass(java.lang.String,byte[],int,int,java.security.ProtectionDomain)
@@ -26,5 +25,8 @@ class FakeAppSpec extends Specification {
       //   module java.base does not "opens java.lang" to unnamed module @672af8eb (ReflectUtils.java:52)
       GuiceApplicationBuilder().build() must throwAn[ExecutionError]
     }.toResult
+    else {
+      GuiceApplicationBuilder().build() must not(throwAn[CreationException])
+    }.pendingUntilFixed("See issue #1")
   }
 }
