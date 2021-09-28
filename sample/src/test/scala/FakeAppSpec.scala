e3b56bb98a4910ffc24270058e0cd501aec721e2
Bùi Việt Thành <thanhbv@sandinh.net>
2021-09-28 22:40:42 +0700

    FakeAppSpec on java 17 on CI: module java.base does not "opens java.lang" to unnamed module

--- sample/src/test/scala/FakeAppSpec.scala
+++ sample/src/test/scala/FakeAppSpec.scala
@@ -1,11 +1,30 @@
+import com.google.common.util.concurrent.ExecutionError
 import com.google.inject.CreationException
 import org.specs2.mutable.Specification
 import play.api.inject.guice.GuiceApplicationBuilder
 
 class FakeAppSpec extends Specification {
+  private def javaVersion: Int = scala.sys
+    .props("java.specification.version")
+    .split('.')
+    .dropWhile(_ == "1")
+    .head
+    .toInt
+
   """FakeApplication (and `WithApplication`) can't start, even with this setting in reference.conf:
     | play.modules.disabled += "play.api.db.evolutions.EvolutionsModule"
     | """.stripMargin >> {
-    GuiceApplicationBuilder().build() must not(throwAn[CreationException])
-  }.pendingUntilFixed("See issue #1")
+
+    if (javaVersion < 16) {
+      GuiceApplicationBuilder().build() must not(throwAn[CreationException])
+    }.pendingUntilFixed("See issue #1")
+    else {
+      // ExecutionError: Fatal execution error, caused by java.lang.ExceptionInInitializerError
+      // CAUSED BY
+      // InaccessibleObjectException: Unable to make protected final java.lang.Class java.lang.ClassLoader.defineClass(java.lang.String,byte[],int,int,java.security.ProtectionDomain)
+      //   throws java.lang.ClassFormatError accessible:
+      //   module java.base does not "opens java.lang" to unnamed module @672af8eb (ReflectUtils.java:52)
+      GuiceApplicationBuilder().build() must throwAn[ExecutionError]
+    }.toResult
+  }
 }
